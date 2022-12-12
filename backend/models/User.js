419addb0c6e9764e4b41ae7cfc5be78d7492aa53
“[Nicholas <n.samaras4@outlook.com>
2022-12-12 16:00:56 -0500

    User.js added

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'company'],
+    default: 'user',
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
