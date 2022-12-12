17455afd1e0158a47ad6e3aeafe045951ce660bf
“[Nicholas <n.samaras4@outlook.com>
2022-12-12 16:00:08 -0500

    update

---
+++ backend/models/Posts.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.SchemaTypeOptions;
+
+const PostSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
