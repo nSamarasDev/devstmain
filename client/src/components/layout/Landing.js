b777a933bbc3a96f2046f48b5225af281359648f
“[Nicholas <n.samaras4@outlook.com>
2023-03-25 19:00:19 -0400

    update

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link, Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+export const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Load Board</h1>
+          <p className="lead">
+            Connect with drivers and a world of oppertunities
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+            <Link to="/contact" className="btn btn-primary">
+              Contact Form
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
